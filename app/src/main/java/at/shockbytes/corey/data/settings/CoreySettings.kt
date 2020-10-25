d81d822df4e02f7c2172091717cb6b5d9a30a7b6
shockbytes <mescht93@gmail.com>
2020-10-25 19:22:14 +0100

    Ad corey settings for dark mode support

---
+++ app/src/main/java/at/shockbytes/corey/data/settings/CoreySettings.kt
@@ -0,0 +1,22 @@
+package at.shockbytes.corey.data.settings
+
+import android.content.Context
+import android.content.SharedPreferences
+import at.shockbytes.corey.R
+import at.shockbytes.corey.common.util.delegate.stringDelegate
+
+/**
+ * Author:  Martin Macheiner
+ * Date:    11.02.2018
+ */
+class CoreySettings(
+    context: Context,
+    prefs: SharedPreferences,
+) {
+
+    private val darkModeString: String by prefs.stringDelegate(context.getString(R.string.prefs_dark_mode_key), defaultValue = "system")
+
+    val themeState: ThemeState
+        get() = ThemeState.ofString(darkModeString) ?: ThemeState.SYSTEM
+
+}
